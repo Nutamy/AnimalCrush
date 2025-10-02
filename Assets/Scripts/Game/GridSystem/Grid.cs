906e328e4b57661aeee14bf3d896106a3f647b0a
Nataliya Kozulina <natasha.pechenka@gmail.com>
2025-10-02 13:16:45 +0500

    Set 002
    add fruits and configs

--- Assets/Scripts/Game/GridSystem/Grid.cs
+++ Assets/Scripts/Game/GridSystem/Grid.cs
@@ -1,6 +1,57 @@
+using Game.Tiles;
 using UnityEngine;
 
-public class Grid
+namespace Game.GridSystem
 {
-    
+    public class Grid
+    {
+        public Tile[,] GameGrid {get; private set;}
+        public int Width { get; private set;}
+        public int Height { get; private set;}
+
+        public Vector2Int CurrentPosition { get; private set;}
+        public Vector2Int TargetPosition { get; private set;}
+
+        public Grid(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            GameGrid = new Tile[Width, Height];
+        }
+
+        public Vector2Int SetCurrentPosition(Vector2Int position) => CurrentPosition = position;
+        public Vector2Int SetTargetPosition(Vector2Int position) => TargetPosition = position;
+
+        public Vector3 GridToWorld(int x, int y) => new Vector3(x, y, 0);
+
+        public Vector2Int WorldToGrid(Vector3 worldPosition)
+        {
+            var x = Mathf.FloorToInt(worldPosition.x);
+            var y = Mathf.FloorToInt(worldPosition.y);
+            return new Vector2Int(x, y);
+        }
+        public void SetValue(int x, int y, Tile tile)
+        {
+            if (IsValidPosition(x, y))
+            {
+                GameGrid[x, y] = tile;
+            }
+        }
+        public void SetValue(Vector3 worldPosition, Tile tile)
+        {
+            var worldToGrid = WorldToGrid(worldPosition);
+            SetValue(worldToGrid.x, worldToGrid.y, tile);
+        }
+
+        public Tile GetValue(int x, int y) => IsValidPosition(x, y) ? GameGrid[x,y] : default;
+
+        public Tile GetValue(Vector3 worldPosition)
+        {
+            var worldToGrid = WorldToGrid(worldPosition);
+            return GetValue(worldToGrid.x, worldToGrid.y);
+        }
+
+        public bool IsValidPosition(int x, int y) => x >= 0 && y >= 0 && x < Width && y < Height;
+    }
 }
+
